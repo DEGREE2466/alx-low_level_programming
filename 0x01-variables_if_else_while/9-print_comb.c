a993f37bcd04c4b96eb4ae725f76e457299e867f
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-12 11:50:23 -0800

    9 print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,18 @@
 
 int main(void)
 {
-	int n;
+	int number;
 
-	for (n = 48; n < 58; n++)
+	for (number = 0; number < 9; number++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(number + '0');
+		if (number < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	putchar('\n');
-	return (0);
 	}
+	putchar('\n');
+
+	return (0);
 }
