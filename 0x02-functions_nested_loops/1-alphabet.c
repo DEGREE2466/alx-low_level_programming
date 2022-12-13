0c839cf3333ac0bff8e3779792398bc10dffda2f
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-13 04:56:04 -0800

    print all apha in small caps

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+
+void print_alphabet(void)
+{
+	cha la;
+
+	for (la = 'a'; la <= 'z'; la++)
+
+	_putchar(la);
+
+	_putchar('\n');
+}
