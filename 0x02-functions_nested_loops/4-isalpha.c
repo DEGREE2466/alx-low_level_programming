a7a689608dcbe982e58c484a70b1f64ff3403e29
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-13 05:46:47 -0800

    ch3ck char both lower and upper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet
+ * @c: character to be checked
+ *
+ * Returns: 1 if the character is a letter, 0 if otherwise
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
