7e4c38d1b2cf8c8a4f43cdde9d4322e120f11103
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-13 05:58:26 -0800

    print all etters in all caps

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
 /**
- * _isalpha - checks for alphabet
+ * _isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Returns: 1 if the character is a letter, 0 if otherwise
+ * Return: 1 if character is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
-
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
