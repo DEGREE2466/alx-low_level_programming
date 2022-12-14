ff26c1053fb81f6771feb6e589146739261f6460
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-13 18:28:19 -0800

    corrected syntax

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,18 +4,18 @@
  */
 void times_table(void)
 {
-	int num, multi, prod;
+	int num, mult, prod;
 
-	for (num = 0, num <= 9, num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
-		for (multi = 1; multi <= 9; multi++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
-			prod = num + multi;
+			prod = num + mult;
 
 			if (prod <= 9)
 				_putchar(' ');
