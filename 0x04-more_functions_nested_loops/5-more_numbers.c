f7a448396fe5fb7cef9366dd7a3c6d9767a59297
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-15 03:34:49 -0800

    5 more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+		}
+		_putchar('\n');
+	}
+}
