6f29e7f27097b292722e8fcdf8e467a38b0fc661
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-15 17:46:47 -0800

    test functions

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int a, int c);
+void print_remaining_days(int month, int day); 
+
+#endif
