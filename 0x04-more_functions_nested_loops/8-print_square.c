a49658c70dd32c14d9c57bd35bc9cd6b6431b68e
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-15 20:35:40 -0800

    prints hashes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h" 
+/**
+ * print_square - prints hashes squares
+ * @size: size of the square
+ * Rturn: no return
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size, i++)
+	{
+		for (j = 0; j < size; j++)
+
+		{
+			_putchar(35);
+		}
+		if (i != size -1)
+			_putchar('\r');
+	}
+	_putchar('\n');
+}
+
