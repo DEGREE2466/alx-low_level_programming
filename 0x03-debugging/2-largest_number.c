ecc0be2ca00fd15f834c1cef0a34bf2d1e563736
degree2466 <uzochukwugodwin959@gmail.com>
2022-12-15 18:16:30 -0800

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * _putchar(i);
+	 * }
+	 */
+	printf("infinite loop avoided: \\o/\n");
+
+	return (0);
+}
--- 0x03-debugging/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int a, int a, int c);
-void print_remaining_days(int month, int day); 
-
-#endif
