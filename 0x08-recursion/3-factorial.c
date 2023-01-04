50fbd75ca1472bf806b9cd8b0c2c9b89eb19e7e2
degree2466 <uzochukwugodwin959@gmail.com>
2023-01-04 10:33:29 -0800

    task 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-factorial.c
+ * Auth: degree2466
+ */
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to find the factorial of
+ *
+ * Return: if n > 0 - the factorial of n
+ *	   if n < 0 - 1 to indicate the erro
+ */
+
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+}
