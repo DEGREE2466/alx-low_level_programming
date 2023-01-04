c568641c8d4ff2c7f7b5b3cf2ee9f6774f0e81b8
degree2466 <uzochukwugodwin959@gmail.com>
2023-01-04 10:54:57 -0800

    the whole code rewritten

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -14,10 +14,9 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	if (*s == '/0')
+		return (0);
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	else
+		return (1 + _strlen_recursion(s + 1)); /*sumof 1*/ 
+}
