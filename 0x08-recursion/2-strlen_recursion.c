dddd589b2278acc0053c4442bba832d4685ecc59
degree2466 <uzochukwugodwin959@gmail.com>
2023-01-04 10:10:08 -0800

    task 3

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: degree2466
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to be measured
+ *
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+}
