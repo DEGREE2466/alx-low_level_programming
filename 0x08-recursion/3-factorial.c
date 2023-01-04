24f6269fb6307a8022eaafea4769576b989f1f43
degree2466 <uzochukwugodwin959@gmail.com>
2023-01-04 11:16:56 -0800

    all code rewritten

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -15,11 +15,12 @@
 
 int factorial(int n)
 {
-	int result = n;
+	if (n == 0) /*Base condition*/
 
-	if (n < 0)
-		return (-1);
-
-	else if (n >= 0 && n <= 1)
 		return (1);
+
+	else if (n < 0) /*Base condition*/
+		return (-1);
+	else
+		return (n * factorial(n - 1)); /*Recurvise call*/
 }
