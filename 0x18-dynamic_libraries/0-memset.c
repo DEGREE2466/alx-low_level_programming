f72e7de75886ceecc694bcc99c157bca4f8ea68e
degree2466 <uzochukwugodwin959@gmail.com>
2023-03-20 20:10:47 -0700

    task 0

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory with a specific value
+ * @s: starting the address of the memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks if number is 0 to 9
+ * @c: char to check
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
