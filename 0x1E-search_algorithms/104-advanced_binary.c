1718166aa9026bdb50edacbd07fc3d15609751fb
degree2466 <uzochukwugodwin959@gmail.com>
2023-07-12 19:15:33 -0700

    task number 11

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_array - prints array of the integers
+ * print_array - print an array of integers
  *
- * @array: the array to be printed
- * @size: the size of the array to be printed
+ * @array: the array to print
+ * @size: the size of the array
  *
  * Return: nothing!
  */
@@ -22,12 +22,12 @@ void print_array(int *array, size_t size)
 }
 
 /**
- * advanced_binary - searching for a value in a list using the binary
- * search algorithm.
+ * advanced_binary - searches for a value in a list using the binary search
+ * algorithm.
  *
- * @array: The array of integers
- * @size: The size of the array
- * @value: The value to search for
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to search for
  *
  * Return: the index of the first @value was found or -1 if it wasn't found.
  */
@@ -40,20 +40,21 @@ int advanced_binary(int *array, size_t size, int value)
 
 	printf("Searching in array: ");
 	print_array(array, size);
-	mid = (size - 1) / 2;
+	mid = size / 2;
 
 	if (value == array[mid])
 	{
-		if ((mid > 0 && array[mid - 1] != value) || (mid == 0))
+		if ((mid == 0) || (array[mid - 1] != value))
 			return (mid);
-		return (advanced_binary(array, size - (mid + 1), value));
+		return (advanced_binary(array, mid + 1, value));
 	}
 	else if (value < array[mid])
-		return (advanced_binary(array, size - (mid + 2), value));
+		return (advanced_binary(array, mid, value));
 
 	index = advanced_binary(
-		array + mid + 1, size - (mid + 1), value);
+		array + mid + 1, size - mid - 1, value);
 	if (index > -1)
-		return ((mid + 1) + index);
+		return (mid + 1) + index;
 	return (-1);
 }
+
